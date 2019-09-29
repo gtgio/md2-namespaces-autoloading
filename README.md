b4420046421002ea558f9b1212f3a7ddbb4b88ee
Hidde Braun <hidde.braun@gmail.com>
2019-09-29 21:03:48 +0200

    Autoloader toegevoegd

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Met de informatie en kennis die je net hebt geleerd over Namespaces:
 2. Verander de `Product` class in de map `other-classes` zodat deze bij de **namespace** `Nike\Commerce\Webshop` hoort.
 3. Je hebt nu twee `Product` classes, die allebei worden ingeladen. **dit geeft een fatal error!**
 4. Verander `index.php` door voor elke class onder de require statements een `use <namespace>\<classname>` toe te voegen.
-5. Laadt ook die **tweede* Product class in uit de `Nike\Commerce\Webshop` namespace en zorg dat er geen conflicten zijn.
+5. Laadt ook de **tweede* Product class in uit de `Nike\Commerce\Webshop` namespace en zorg dat er geen conflicten zijn.
 6. Uncomment de regels met het `$nike_product` en kijk of het nu wel werkt.
 7. Maak nu een map aan met de naam `src`. Kijk naar de namespaces van de classes en maak de juiste mappen aan zoals in de slides als voorbeeld is gegeven. 
     * Dus voor elk deel van de namespace een nieuwe map
--- index.php
+++ index.php
@@ -1,39 +1,18 @@
 <?php
-ini_set('display_errors',1); //Om foutberichten altijd te tonen (handig op Ma cloud)
+ini_set('display_errors', 1); //Om foutberichten altijd te tonen (handig op Ma cloud)
 
 require 'classes/ProductCatalogue.php';
 require 'classes/Product.php';
 require 'classes/ShoppingCart.php';
 //require 'other-classes/Product.php';
 
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Namespaces</title>
-</head>
-<body>
-<?php
+$product = new Product('Broorje Gezond');
+//$nike_product = new Product('Air Max',159,95);
 
-$product   = new Product('Broorje Gezond');
-//$nike_product = new NikeProduct('Air Max',159,95);
-
-$cart      = new ShoppingCart();
+$cart = new ShoppingCart();
 $catalogue = new ProductCatalogue();
 
 echo $product->getName();
 //echo $nike_product->getName();
 
-// Deze bestanden doen verder niets, dit gaat er puur om dat je begrijpt wat namespaces zijn
-// En hoe je je bestanden met classes organiseert.
-
-
-
-?>
-
-</body>
-</html>
+// Deze bestanden doen verder niet veel bijzonders. Het is alleen om te oefenen met namespaces en autoloading
