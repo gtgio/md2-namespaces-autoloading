426722707fbc9fd6e440b737c7d718b9433e4998
Giovanni <43673511+gtgio@users.noreply.github.com>
2020-10-23 10:06:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
 ini_set('display_errors', 1); //Om foutberichten altijd te tonen (handig op Ma cloud)
+require 'autoload.php';
+// require 'src/classes/ProductCatalogue.php';
+// require 'src/classes/Product.php';
+// require 'src/classes/ShoppingCart.php';
 
-require 'classes/ProductCatalogue.php';
-require 'classes/Product.php';
-require 'classes/ShoppingCart.php';
-//require 'other-classes/Product.php';
+use Gio\BAP\Product;
+use Gio\BAP\ShoppingCart;
+use Gio\BAP\ProductCatalogue;
+
+// require 'src/Product.php';
+
+use Nike\Commerce\Webshop\Product as NikeProduct;
 
 $product = new Product('Broodje Gezond');
-//$nike_product = new Product('Air Max',159.95);
+$nike_product = new NikeProduct('Air Max',159.95);
 
 $cart = new ShoppingCart();
 $catalogue = new ProductCatalogue();
 
 echo $product->getName();
-//echo $nike_product->getName();
+echo $nike_product->getName();
 
 // Deze bestanden doen verder niet veel bijzonders. Het is alleen om te oefenen met namespaces en autoloading
