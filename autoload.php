2454af5f0650600ae30acbf805e164bf5cc90780
Giovanni <43673511+gtgio@users.noreply.github.com>
2020-10-23 10:06:47 +0200

    Add files via upload

--- autoload.php
+++ autoload.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Deze functie wordt elke keer als je nieuwe instance maakt met 'new <ClassName>()' door PHP uitgevoerd
- * De functie zoekt het juiste PHP bestand op voor de class en laadt het in met een require.
- * Dit is dus een Autoloader. Deze Autoloader zorgt er voor dat automatisch het juiste bestand wordt ingeladen
- * Dit voorkomt dat je overal heel veel require statements moet schrijven die classes inladen in je code!
+ * Deze functie wordt elke keer als je nieuwe instance maakt met 'new <ClassName>()' door PHP uitgeveord
+ * De functie zoekt het juiste bestand op voor de class en laadt het in met require.
+ * Dit is dus een Autoloader. Deze Autoloader zorgt er voor dat automatisch het juiste bestand wordt geladen
+ * Zodat je niet zelf alle classes hoeft op te geven met require.
  *
  * @param $class string De naam van de class die geladen moet worden
  */
@@ -12,17 +12,16 @@ function autoload_class($class)
 {
     $base_dir = __DIR__ . '/src/';
 
-    // Vervangt de slashes: "\" in de namespace prefix met een "/" en zet er ".php" achter.
-    // Wat gebeurt er dan denk je? Waar zoekt het script nu naar de class, in welk bestand
-    // Gebruik een echo om te "debuggen" vanaf waar PHP de class probeert in te laden.
-
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the class name, append
+    // with .php
     $file = $base_dir . str_replace('\\', '/', $class) . '.php';
 
-    // Als het bestand bestaat, laadt het in
+    // if the file exists, require it
     if (file_exists($file)) {
         require $file;
     }
 }
 
-// Dit stelt PHP in zodat de function 'autoload_class' (hierboven) gebruikt wordt voor het laden van classes
+// Vertel PHP dat de function 'autoload_class' gebruikt moet worden voor het laden van classes
 spl_autoload_register('autoload_class');
